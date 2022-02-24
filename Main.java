af06b7af4af6f7d4339a99827d82e4e8382ab9a1
Felipe <felipemoreira1@gmail.com>
2022-02-24 15:17:52 -0300

    Stage 2/4: Convert to decimal
    Adicionei a conversão no outro sentido e coloquei o programa em um loop
    para terminar apenas quando solicitado pelo usuário.

--- Main.java
+++ Main.java
@@ -2,18 +2,39 @@ import java.util.Scanner;
 
 public class Main {
 
+    private static Scanner scanner;
+
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+        scanner = new Scanner(System.in);
+        String command = "";
+        do {
+            System.out.print("\nDo you want to convert /from decimal or /to decimal? (To quit type /exit) ");
+            command = scanner.nextLine();
+            
+            switch(command) {
+                case "/from":
+                    convertFromDec();
+                    break;
+                case "/to":
+                    convertToDec();
+                    break;
+                default:
+                    continue;
+
+            }
+            System.out.println();
+        } while ("/exit".compareTo(command) != 0);
+        
+        scanner.close();
+
+    }
+
+    public static void convertFromDec() {
         System.out.print("Enter number in decimal system: ");
         int decNumber = scanner.nextInt();
         System.out.print("Enter target base: ");
         int targetBase = scanner.nextInt();
-        System.out.printf("Conversion result: %s\n", convert(decNumber, targetBase));
-
-    }
-
-    public static String convert(int decNumber, int targetBase) {
 
         String result = "";
         int number = decNumber;
@@ -52,6 +73,50 @@ public class Main {
             number /= targetBase;
         }
 
-        return result;
+        System.out.printf("Conversion result: %s", result);
+    }
+
+    public static void convertToDec() {
+        System.out.print("Enter source number: ");
+        String sourceNumber = scanner.nextLine();
+        System.out.print("Enter source base: ");
+        int sourceBase = scanner.nextInt();
+
+        int result = 0;
+        int power = 0;
+        sourceNumber = new StringBuilder(sourceNumber).reverse().toString();
+        for(char c : sourceNumber.toCharArray()) {
+            
+            int digit = 0;
+            switch(c) {
+                case 'A':
+                    digit = 10;
+                    break;
+                case 'B':
+                    digit = 11;
+                    break;
+                case 'C':
+                    digit = 12;
+                    break;
+                case 'D':
+                    digit = 13;
+                    break;
+                case 'E':
+                    digit = 14;
+                    break;
+                case 'F':
+                    digit = 15;
+                    break;
+                default:
+                    digit = Character.getNumericValue(c);
+            }
+
+            int multiplicator = (int) Math.pow(sourceBase, power);
+            digit *= multiplicator;
+            result += digit;
+            power++;
+        }
+
+        System.out.printf("Conversion to decimal result: %d", result);
     }
 }
